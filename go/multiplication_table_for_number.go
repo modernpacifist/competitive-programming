5a1125e642811b6677a083e4d2b2a7e791fe5d1f
modernpacifist <modernpacifist@gmail.com>
2021-10-11 03:00:02 +0300

    11/10/2021-03:00:02 automated backup commit

--- go/multiplication_table_for_number.go
+++ go/multiplication_table_for_number.go
@@ -1,12 +1,25 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "strconv"
+)
+
 
 func MultiTable(number int) string {
-    return ""
+    var a string
+    for i := 1; i <= 10; i++ {
+        a += strconv.Itoa(i)
+        a += " * "
+        a += strconv.Itoa(number)
+        a += " = "
+        a += strconv.Itoa(i * number)
+        a += "\n"
+    }
+    return a
 }
 
 func main() {
-    fmt.Println("hello")
+    fmt.Println(MultiTable(5))
 }
 
