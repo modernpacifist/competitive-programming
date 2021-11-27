c724a5a2d80231e9b8e8e182b57ed9e52bea3a9e
modernpacifist <modernpacifist@gmail.com>
2021-11-28 02:25:59 +0300

    cpp/balanced-number

--- cpp/balanced-number.cpp
+++ cpp/balanced-number.cpp
@@ -3,24 +3,53 @@
 
 
 std::string balancedNum(unsigned long long int number) {
-
     std::string string_number = std::to_string(number);
 
-    if (string_number.length() == 1) {
+    int string_length = string_number.length();
+
+    if (string_length == 1) {
         return "Balanced";
     }
 
-    if (string_number.length() == 1) {
-        return "Balanced";
-    }
+    if (string_length % 2 == 0) {
+        int left_sum = 0, right_sum = 0;
 
-    return "Not Balanced";
+        for (int i = 0; i < string_length / 2 - 1; ++i) {
+            left_sum += string_number[i] - 48;
+        }
+
+        for (int i = string_length / 2 + 1; i < string_length; ++i) {
+            right_sum += string_number[i] - 48;
+        }
+
+        if (left_sum == right_sum) {
+            return "Balanced";
+        }
+
+        return "Not Balanced";
+
+    } else {
+        int left_sum = 0, right_sum = 0;
+
+        for (int i = 0; i < string_length / 2; ++i) {
+            left_sum += string_number[i] - 48;
+        }
+
+        for (int i = string_length / 2 + 1; i < string_length; ++i) {
+            right_sum += string_number[i] - 48;
+        }
+
+        if (left_sum == right_sum) {
+            return "Balanced";
+        }
+
+        return "Not Balanced";
+    }
 }
 
 
 int main() {
-    std::cout << balancedNum(1) << std::endl;
-    std::cout << balancedNum(10) << std::endl;
+    std::cout << balancedNum(1301) << std::endl;
 
     return 0;
 }
