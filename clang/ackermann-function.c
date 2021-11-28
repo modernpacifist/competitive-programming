793b3fc33e1a7314ab64838aa3c7ebdee7fb17fd
modernpacifist <modernpacifist@gmail.com>
2021-11-28 22:57:31 +0300

    clang/ackermann-function

--- clang/ackermann-function.c
+++ clang/ackermann-function.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
 
+int Ackermann(int m, int n) {
+    if (m < 0 || n < 0) {
+        return -1;
+    }
+
+    if (m == 0) {
+        return n + 1;
+    }
+
+    if (m > 0 && n == 0) {
+        return Ackermann(m - 1, 1);
+    }
+
+    if (m > 0 && n > 0) {
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    }
+
+    return 0;
+}
+
+
 int main() {
-    printf("%s", "Hello world");
+    int ackermann_res = Ackermann(1,3);
+
+    printf("%d\n", ackermann_res);
+
     return 0;
 }
