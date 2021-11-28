163e09676e87e5dffa8c9f72ba012e6c54220723
modernpacifist <modernpacifist@gmail.com>
2021-11-28 21:00:01 +0300

    28/11/2021-21:00:01 automated backup commit

--- cpp/sort-the-odd.cpp
+++ cpp/sort-the-odd.cpp
@@ -1,14 +1,33 @@
-#include <cstdio>
-#include <vector>
+#include <algorithm>
+#include <iostream>
 
 
-std::vector<int> sortArray(std::vector<int> array) {
+std::vector<int> sortArray(std::vector<int> a) {
+    std::vector<int> odd_values;
+    std::vector<int> odd_positions;
 
-    return array;
+    for (size_t i = 0; i < a.size(); ++i) {
+        if (a[i] % 2 != 0) {
+            odd_values.push_back(a[i]);
+            odd_positions.push_back(i);
+        }
+    }
+
+    std::sort(odd_values.begin(), odd_values.end());
+
+    for (size_t i = 0; i < odd_positions.size(); ++i) {
+        a[odd_values[i]]
+    }
+
+    return a;
 }
 
 
 int main() {
+    std::vector<int> expected = {1, 3, 2, 8, 5, 4};
+    std::vector<int> a = {5, 3, 2, 8, 1, 4};
+
+    sortArray(a);
 
     return 0;
 }
