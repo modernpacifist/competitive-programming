2196701fc0a403b06c1822067998694c096705d1
modernpacifist <modernpacifist@gmail.com>
2021-11-28 19:08:45 +0300

    cpp/special-number refactor

--- cpp/special-number.cpp
+++ cpp/special-number.cpp
@@ -6,15 +6,12 @@
 const char* specialNumber(int n) {
     bool flag = true;
 
-    if (0 <= n && n <= 5) {
-    } else {
-        std::string string_number = std::to_string(n);
-
-        for (size_t i = 0; i < string_number.length(); ++i) {
-            if (!(0 <= (int)(string_number[i]) - 48 && (int)(string_number[i]) - 48 <= 5)) {
-                flag = false;
-            }
+    while (n > 0) {
+        if (n % 10 > 5) {
+            flag = false;
+            break;
         }
+        n /= 10;
     }
 
     return (flag) ? "Special!!" : "NOT!!";
@@ -22,6 +19,9 @@ const char* specialNumber(int n) {
 
 
 int main() {
+    std::cout << specialNumber(2) << std::endl;
+    std::cout << specialNumber(3) << std::endl;
+    std::cout << specialNumber(9) << std::endl;
     std::cout << specialNumber(23) << std::endl;
 
     return 0;
