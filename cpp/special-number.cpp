7224c2e2e50329c3ee6cb6d8aaaa8d9d673d80f2
modernpacifist <modernpacifist@gmail.com>
2021-11-28 19:02:33 +0300

    cpp/special-number

--- cpp/special-number.cpp
+++ cpp/special-number.cpp
@@ -3,27 +3,16 @@
 #include <vector>
 
 
-//std::vector<int> int_to_arr(int n) {
-    //std::vector<int> arr = {0};
-    //std::string num_string;
-    //while (n > 0) {
-    //}
-    //return {0};
-//}
-
-
 const char* specialNumber(int n) {
     bool flag = true;
 
     if (0 <= n && n <= 5) {
     } else {
-        std::cout << "HHEE" << std::endl;
         std::string string_number = std::to_string(n);
 
         for (size_t i = 0; i < string_number.length(); ++i) {
-            if (!(0 <= (int)(string_number[i]) && (int)(string_number[i]) <= 5)) {
+            if (!(0 <= (int)(string_number[i]) - 48 && (int)(string_number[i]) - 48 <= 5)) {
                 flag = false;
-                break;
             }
         }
     }
@@ -33,8 +22,7 @@ const char* specialNumber(int n) {
 
 
 int main() {
-    //std::cout << specialNumber(5) << std::endl;
-    //std::cout << specialNumber(6) << std::endl;
     std::cout << specialNumber(23) << std::endl;
+
     return 0;
 }
