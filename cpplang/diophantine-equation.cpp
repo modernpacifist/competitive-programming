a3e7b48e82723219c11ffc33785022472ac50195
modernpacifist <modernpacifist@gmail.com>
2021-11-29 12:23:31 +0300

    cpplang/diophantine-equation wip

--- cpplang/diophantine-equation.cpp
+++ cpplang/diophantine-equation.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <utility>
+#include <cmath>
+
+
+class Dioph {
+    public:
+        static std::vector<std::pair <long, long>> solEquaStr(long long n) {
+            std::vector<std::pair <long, long>> res;
+            std::pair<long, long> poly_pair;
+            long long x = n, y;
+
+            // x^2 - 4 * y^2 = n
+            while (x >= 0 || y >= 0) {
+                y = x;
+                
+                if (pow(x, 2) - 4 * pow(y, 2) == n) {
+                    poly_pair = std::make_pair(x, y);
+
+                    res.push_back(poly_pair);
+                }
+
+                x--;
+            }
+
+            return res;
+        }
+};
 
 
 int main() {
+    Dioph d;
 
-    std::cout << "hello world" << std::endl;
+    for (auto i : d.solEquaStr(90005)) {
+        std::cout << i << std::endl;
+    }
 
     return 0;
 }
