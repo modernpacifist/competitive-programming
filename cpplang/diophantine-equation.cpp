a1bd7551d65c841eb8dba867687c521e3109de67
modernpacifist <modernpacifist@gmail.com>
2021-12-05 15:00:02 +0300

    05/12/2021-15:00:02 automated backup commit

--- cpplang/diophantine-equation.cpp
+++ cpplang/diophantine-equation.cpp
@@ -5,18 +5,24 @@
 
 
 class Dioph {
+    private:
+        static int gcd(int a, int b) {
+            return (a % b == 0) ? abs(b) : gcd(b, a % b);
+        }
+
     public:
         static std::vector<std::pair <long, long>> solEquaStr(long long n) {
             std::vector<std::pair <long, long>> res;
             std::pair<long, long> poly_pair;
 
             // x^2 - 4 * y^2 = n
-            for (long long x = (n / 2) + 1; x > 0; x /= 2) {
+            for (long long x = (n / 2) + 1; x > 0; x /= 8) {
                 for (long long y = x / 2; y > 0; y--) {
                     if (pow(x, 2) - 4 * pow(y, 2) == n) {
                         poly_pair = std::make_pair(x, y);
 
                         res.push_back(poly_pair);
+                        std::cout << poly_pair.first << " " << poly_pair.second << std::endl;
                     }
                 }
             }
