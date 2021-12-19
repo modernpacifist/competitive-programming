722be502cec5ab3e0b7ec713dd072f6de9e3addd
modernpacifist <modernpacifist@gmail.com>
2021-12-19 06:00:01 +0300

    19/12/2021-06:00:01 automated backup commit

--- cpplang/valid-braces.cpp
+++ cpplang/valid-braces.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
 #include <string>
+#include <map>
 
 
-bool valid_braces(std::string braces) {
-    if (braces.length() == 2) {
-        return true;
+bool valid_braces(std::string braces_string) {
+    if (braces_string.length() % 2 == 1) {
+        return false;
     }
-    return false;
+
+    //std::map<char, int> braces_dict;
+
+    //for (auto i : braces_string) {
+        //if (braces_dict.count(i)) {
+            //++braces_dict[i];
+        //} else {
+            //braces_dict.insert({i, 1});
+        //}
+    //}
+
+    for (auto i : braces_string) {
+        switch (i) {
+            case '(':
+                <code>
+                break;
+            default:
+                <default_case>
+        }
+    }
+
+    return true;
 }
 
 
 int main() {
-    const std::string test {"fda"};
+    const std::string test {"(){}[]"};
 
     std::cout << valid_braces(test) << std::endl;
 
