1423aebfd416506ecc09746868d5559657e3996d
modernpacifist <modernpacifist@gmail.com>
2021-12-25 15:51:52 +0300

    cpplang/sum-of-odd-numbers.cpp

---
+++ cpplang/sum-of-odd-numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+
+long long rowSumOddNumbers(unsigned n) {
+    return n*n*n;
+}
+
+
+int main() {
+    std::cout << rowSumOddNumbers(4) << std::endl;
+    
+    return 0;
+}
