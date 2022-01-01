e3a8783cc41fa117447faa62517a1e3f1a932b05
modernpacifist <modernpacifist@gmail.com>
2022-01-01 16:11:41 +0300

    cpplang/geometry-basics-cross-product-in-3d.cpp

---
+++ cpplang/geometry-basics-cross-product-in-3d.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+class Vector {
+public:
+    int x, y, z;
+
+    Vector(int x, int y, int z) {
+        this -> x = x;
+        this -> y = y;
+        this -> z = z;
+    };
+};
+
+
+Vector cross_product(const Vector& a, const Vector& b){
+    return Vector(a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x);
+}  
+
+
+int main() {
+    Vector a = Vector(2, 0, -1);
+    Vector b = Vector(0, -2, 1);
+
+    
+    return 0;
+}
