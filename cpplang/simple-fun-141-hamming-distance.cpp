67f72e39509bedf504563d3e321c2b53f8b6511e
modernpacifist <modernpacifist@gmail.com>
2022-01-05 21:00:02 +0300

    05/01/2022-21:00:02 automated backup commit

--- cpplang/simple-fun-141-hamming-distance.cpp
+++ cpplang/simple-fun-141-hamming-distance.cpp
@@ -4,18 +4,13 @@
 
 
 std::uint32_t hamming_distance(std::uint32_t a, std::uint32_t b) {
-    std::uint32_t r = 0, c = (a ^ b);
-    while (c) {
-        r += c & 1;
-        c >>= 1;
-    }
-    return r;
+    return __builtin_popcount(a ^ b);
 }
 
 
 int main() {
     std::cout << hamming_distance(12, 15) << std::endl;
-    //std::cout << hamming_distance(25, 87) << std::endl;
+    std::cout << hamming_distance(25, 87) << std::endl;
     
     return 0;
 }
