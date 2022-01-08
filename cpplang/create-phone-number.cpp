a57a5fce55c3141b65ffcdac8a6208e0eabf2f6f
modernpacifist <modernpacifist@gmail.com>
2022-01-08 20:09:31 +0300

    cpplang/create-phone-number.cpp

--- cpplang/create-phone-number.cpp
+++ cpplang/create-phone-number.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 
-std::string createPhoneNumber(const int arr [10]) {
+std::string createPhoneNumber(const int arr[10]) {
     char buff[15];
-    std::sprintf(buff, sizeof(buff), "(%d%d%d) %d%d%d-%d%d%d%d%d", arr[0], arr[1], arr[2], arr[3], arr[4], 
+    std::sprintf(buff, "(%d%d%d) %d%d%d-%d%d%d%d%d", arr[0], arr[1], arr[2], arr[3], arr[4], 
             arr[5], arr[6], arr[7], arr[8], arr[9]);
     return buff;
 }
