fe8606ccd8ed4058d21424ead68c003fe20b99d8
modernpacifist <modernpacifist@gmail.com>
2022-01-08 20:00:01 +0300

    08/01/2022-20:00:01 automated backup commit

--- cpplang/create-phone-number.cpp
+++ cpplang/create-phone-number.cpp
@@ -1,24 +1,12 @@
 #include <iostream>
 #include <string>
-#include <array>
 
 
 std::string createPhoneNumber(const int arr [10]) {
-    std::string res = "(";
-    int i = 0;
-    for (i = 0; i < 3; ++i) {
-        res.append(std::to_string(arr[i]));
-    }
-    res.append(")");
-    res.append(" ");
-    for (; i < 6; ++i) {
-        res.append(std::to_string(arr[i]));
-    }
-    res.append("-");
-    for (; i < 10; ++i) {
-        res.append(std::to_string(arr[i]));
-    }
-    return res;
+    char buff[15];
+    std::sprintf(buff, sizeof(buff), "(%d%d%d) %d%d%d-%d%d%d%d%d", arr[0], arr[1], arr[2], arr[3], arr[4], 
+            arr[5], arr[6], arr[7], arr[8], arr[9]);
+    return buff;
 }
 
 
