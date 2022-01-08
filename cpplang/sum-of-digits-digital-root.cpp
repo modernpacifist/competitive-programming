670aacb2d96773d355ab53aa887e019c752a7d05
modernpacifist <modernpacifist@gmail.com>
2022-01-08 21:00:01 +0300

    08/01/2022-21:00:01 automated backup commit

--- cpplang/sum-of-digits-digital-root.cpp
+++ cpplang/sum-of-digits-digital-root.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 
 
+//int digital_root(int n) {
+    //int m = 0, s = 0;
+    //while (n > 0) {
+        //m = n % 10;
+        //s += m;
+        //n /= 10;
+    //}
+    //if (s > 10) {
+        //return digital_root(s);
+    //}
+    //return s;
+//}
+
+
 int digital_root(int n) {
-    int m = 0, s = 0;
-    while (n > 0) {
-        m = n % 10;
-        s += m;
-        n /= 10;
-    }
-    if (s > 10) {
-        return digital_root(s);
-    }
-    return s;
+    return --n % 9 + 1;
 }
 
 
