69a22f0032a14ad3a5977879c4856435c181b4c3
modernpacifist <modernpacifist@gmail.com>
2022-03-13 23:00:01 +0300

    13/03/2022-23:00:01 automated backup commit

--- python3/NewModifyDelete.py
+++ python3/NewModifyDelete.py
@@ -1,28 +1,55 @@
 #!/bin/env python3.9
 
 
+class Pair:
+    def __init__(self, one, two):
+        self.first = one
+        self.second = two
+
+
 def getNew(a, b):
     diff = []
-    for a1 in a:
-        if not a1 in b:
-            diff.append(a1)
+    for i in range(len(b)):
+        if b[i].first != a[i].first:
+            diff.append(a[i])
     return diff
 
 
 def getDelete(a, b):
     diff = []
+    for i in range(len(a)):
+        if b[i].first != a[i].first:
+            diff.append(b[i])
+    return list(set(diff))
+
+
+def getModify(a, b):
+    diff = []
+    for i in a:
+        for j in b:
+            if i.first == j.first and i.second != j.second:
+                diff.append(i)
     return diff
 
 
+def printPairList(Diff):
+    for i in Diff:
+        print(i.__dict__, end=" ")
+    print()
+
+
 def main():
-    input = [(1, 132124), (2, 34324), (3, 2342)]
-    output = [(1, 13224), (2, 34324), (4, 34242)]
+    A = [Pair(1, 132124), Pair(2, 34324), Pair(3, 2342)]
+    B = [Pair(1, 13224), Pair(2, 34324), Pair(4, 34242)]
 
-    print("New files:")
-    print(getNew(input, output))
+    print("New:")
+    printPairList(getNew(A, B))
 
-    print("Files to delete:")
-    print(getDelete(input, output))
+    print("To delete:")
+    printPairList(getDelete(A, B))
+
+    print("To modify:")
+    printPairList(getModify(A, B))
 
 
 if __name__ == "__main__":
