c8ab893519dbcc56ce0b24a348a426083a12bb5c
modernpacifist <modernpacifist@gmail.com>
2022-03-27 03:46:31 +0300

    golang/sum-of-integers-in-string.go

--- golang/sum-of-integers-in-string.go
+++ golang/sum-of-integers-in-string.go
@@ -1,11 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
 
-func SumOfIntegersInString(strng string) int {
-  return -1
+func SumOfIntegersInString(strArg string) (res int) {
+	for _, v := range regexp.MustCompile(`[0-9]+`).FindAllString(strArg, -1) {
+		i, _ := strconv.Atoi(v)
+		res += i
+	}
+	return
 }
 
 func main() {
-	fmt.Println()
+	fmt.Println(SumOfIntegersInString("The30quick20brown10f0x1203jumps914ov3r1349the102l4zydog"))
 }
