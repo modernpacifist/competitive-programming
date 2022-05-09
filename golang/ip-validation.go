206afc7ccb10c43858b701615c6af1b240ca555e
modernpacifist <modernpacifist@gmail.com>
2022-05-09 19:55:01 +0300

    09/05/2022-19:55:01 automated backup commit

--- golang/ip-validation.go
+++ golang/ip-validation.go
@@ -1,15 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
 
 func Is_valid_ip(ip string) bool {
+	flag := true
+	r := regexp.MustCompile(`(?P<first>[0-9]{1,3}).(?P<second>[0-9]{1,3}).(?P<third>[0-9]{1,3}).(?P<fourth>[0-9]{1,3})`)
+	names := r.SubexpNames()
 
-	return false
+	result := r.FindAllStringSubmatch("1.2.3.4", -1)
+	m := map[string]string{}
+	for i, n := range result[0] {
+		m[names[i]] = n
+	}
+	for _, v := range m {
+		//fmt.Println(v)
+		i, _ := strconv.Atoi(v)
+		fmt.Println(i)
+		if (i > 256 && i < 1) {
+			flag = false
+		}
+	}
+
+	return flag
 }
 
 func main() {
-	fmt.Println(Is_valid_ip("1.2.3"))
-	fmt.Println(Is_valid_ip("1.2.3.4.5"))
-	fmt.Println(Is_valid_ip("123.456.78.90"))
-	fmt.Println(Is_valid_ip("123.045.067.089"))
+	//r := regexp.MustCompile(`(?P<first>[0-9]{1,3}).(?P<second>[0-9]{1,3}).(?P<third>[0-9]{1,3}).(?P<fourth>[0-9]{1,3})`)
+	//names := r.SubexpNames()
+
+	//result := r.FindAllStringSubmatch("1.2.3.4", -1)
+	//m := map[string]string{}
+	//for i, n := range result[0] {
+		//m[names[i]] = n
+	//}
+
+	//fmt.Println(m["first"])
+	//fmt.Println(m["second"])
+	//fmt.Println(m["third"])
+	//fmt.Println(m["fourth"])
+
+	fmt.Println(Is_valid_ip("1.2.3.-4"))
+	//fmt.Println(Is_valid_ip("1.2.3. 4"))
+	//fmt.Println(Is_valid_ip("1.2.3.4"))
+	//fmt.Println(Is_valid_ip("1.2.3.4.5"))
+	//fmt.Println(Is_valid_ip("123.456.78.90"))
+	//fmt.Println(Is_valid_ip("123.045.067.089"))
 }
