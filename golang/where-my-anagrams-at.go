86bd5666e8e87786f819c316c6141d4f0516bfd2
modernpacifist <modernpacifist@gmail.com>
2022-07-08 20:43:06 +0300

    golang:where-my-anagrams-at.go

--- golang/where-my-anagrams-at.go
+++ golang/where-my-anagrams-at.go
@@ -1,12 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func Anagrams(word string, words []string) []string {
-	return nil
+	res := make([]string, 0)
+	baseWordBuffer := make(map[rune]int, 0)
+
+	for _, c := range word {
+		baseWordBuffer[c]++
+	}
+
+	for _, w := range words {
+		wordBuffer := make(map[rune]int, 0)
+		for _, c := range w {
+			wordBuffer[c]++
+		}
+		eq := reflect.DeepEqual(baseWordBuffer, wordBuffer)
+		if eq {
+			res = append(res, w)
+		}
+	}
+
+	if len(res) == 0 {
+		return nil
+	}
+	return res
 }
 
 func main() {
 	// sample text
-	fmt.Println(Anagrams())
+	fmt.Println(Anagrams("abba", []string{"baba", "abab"}))
+	fmt.Println(Anagrams("abba", []string{"bdba", "dbab"}))
 }
