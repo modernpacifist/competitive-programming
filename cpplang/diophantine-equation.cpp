81b6e991710359a39c040db119733ae364d8e0cf
modernpacifist <modernpacifist@gmail.com>
2022-08-27 14:14:42 +0300

    backup of old changes

--- cpplang/diophantine-equation.cpp
+++ cpplang/diophantine-equation.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <utility>
+
+class Dioph {
+    public:
+    static std::vector<std::pair <long, long>> solEquaStr(long long n);
+};
+
 
 
 int main() {
+    auto Dioph d;
 
     std::cout << "hello world" << std::endl;
 
