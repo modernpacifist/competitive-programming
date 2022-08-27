d05ef67dcfac6e71237aa1ed7ef966c065b30caf
modernpacifist <modernpacifist@gmail.com>
2022-08-27 20:55:01 +0300

    27/08/2022-20:55:01 automated backup commit

--- cpplang/reverse-words.cpp
+++ cpplang/reverse-words.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
+#include <cstring>
 
 
 std::string reverse_words(std::string str) {
-  return "";
+    std::string result_string;
+
+    for (auto i : str) {
+        if (isspace(i)) {
+            std::cout << i << '\n';
+        }
+    }
+
+    return result_string;
 }
 
 
 int main() {
-    std::cout << reverse_words() << '\n';
+    std::cout << reverse_words("hehe sample") << '\n';
     
     return 0;
 }
