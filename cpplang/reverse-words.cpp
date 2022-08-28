580d9da1f650158bd68c14a2d642a692827a3b5d
modernpacifist <modernpacifist@gmail.com>
2022-08-28 06:55:01 +0300

    28/08/2022-06:55:01 automated backup commit

--- cpplang/reverse-words.cpp
+++ cpplang/reverse-words.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <cstring>
+#include <algorithm>
 
 
 std::string reverse_words(std::string str) {
     std::string result_string;
 
-    for (auto i : str) {
+    for (char i : str) {
         std::string buffer_string;
-        if (isspace(i)) {
-            std::cout << i << '\n';
+        if (!isspace(i)) {
+            buffer_string += i;
         } else {
-            
+            std::reverse(buffer_string.begin(), buffer_string.end());
+            std::cout << buffer_string << '\n';
+            result_string += buffer_string;
+            buffer_string.clear();
         }
     }
 
