66f1709d4274acca3b1057b09dccc1bebeb62400
modernpacifist <modernpacifist@gmail.com>
2022-08-29 19:55:01 +0300

    29/08/2022-19:55:01 automated backup commit

---
+++ cpplang/find-the-unique-number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+
+
+float find_uniq(const std::vector<float> &v) {
+    float res = 0;
+    float temp = 0;
+
+    return res;
+}
+
+
+int main() {
+    std::vector<float> a {1,1,2,1,1};
+
+    find_uniq(a);
+    
+    return 0;
+}
