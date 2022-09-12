8278c48b371471b248503c853de473cfc71783fe
modernpacifist <modernpacifist@gmail.com>
2022-09-12 18:55:01 +0300

    12/09/2022-18:55:01 automated backup commit

---
+++ cpplang/dna-to-rna-conversion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+
+std::string DNAtoRNA(std::string dna){
+    return "";
+}
+
+
+int main() {
+    std::cout << DNAtoRNA("GCAT") << '\n';
+    
+    return 0;
+}
