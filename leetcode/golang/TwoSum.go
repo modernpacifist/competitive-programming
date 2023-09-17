ebc9d55394016279ae8857e090de2467ee8487ef
modernpacifist <modernpacifist@gmail.com>
2023-09-17 17:41:10 +0300

    leetcode/golang:TwoSum.go

--- leetcode/golang/TwoSum.go
+++ leetcode/golang/TwoSum.go
@@ -3,19 +3,20 @@ package main
 import "fmt"
 
 func twoSum(nums []int, target int) []int {
-	j := 0
-
 	for i := 0; i < len(nums); i++ {
-		if nums[i] + nums[j] == target {
-			return []int{i, j}
+		for j := i+1; j < len(nums); j++ {
+			if nums[i] + nums[j] == target {
+				return []int{i, j}
+			}
 		}
 	}
-
 	return []int{0, 0}
 }
 
 func main() {
 	nums := []int{2, 7, 11, 15, 9}
-
 	fmt.Println(twoSum(nums, 9))
+
+	nums = []int{3, 2, 4}
+	fmt.Println(twoSum(nums, 6))
 }
