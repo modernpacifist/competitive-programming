113c330abf37d70a546f8b74cf319d36f26b6530
modernpacifist <modernpacifist@gmail.com>
2023-09-17 16:55:01 +0300

    proelium-exercitum:17/09/2023-16:55:01 automated backup commit

--- leetcode/golang/TwoSum.go
+++ leetcode/golang/TwoSum.go
@@ -3,11 +3,19 @@ package main
 import "fmt"
 
 func twoSum(nums []int, target int) []int {
+	j := 0
 
+	for i := 0; i < len(nums); i++ {
+		if nums[i] + nums[j] == target {
+			return []int{i, j}
+		}
+	}
+
+	return []int{0, 0}
 }
 
 func main() {
-	nums int = [2, 7, 11, 15]
+	nums := []int{2, 7, 11, 15, 9}
 
-	fmt.Println(twoSum)
+	fmt.Println(twoSum(nums, 9))
 }
