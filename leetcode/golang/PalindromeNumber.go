88d9d77a96deb47979a5dcb6aa8b3c8aa912ad13
modernpacifist <modernpacifist@gmail.com>
2023-09-18 09:36:20 +0300

    leetcode/golang:PalindromeNumber.go

--- leetcode/golang/PalindromeNumber.go
+++ leetcode/golang/PalindromeNumber.go
@@ -1,15 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
+
+func reverse(str string) (result string) {
+	for _, v := range str {
+		result = string(v) + result
+	}
+	return
+}
 
 func isPalindrome(x int) bool  {
 	if x < 0 {
 		return false
 	}
-	return true
+
+	s2 := strconv.Itoa(x)
+
+	return reverse(s2) == s2
 }
 
 func main() {
 	fmt.Println(isPalindrome(121))
 	fmt.Println(isPalindrome(-121))
+	fmt.Println(isPalindrome(1488))
+	fmt.Println(isPalindrome(8841488))
 }
