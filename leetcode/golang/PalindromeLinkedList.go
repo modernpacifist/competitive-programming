663634198682885a0de2cf4860eb7dcd89070de3
modernpacifist <modernpacifist@gmail.com>
2023-09-26 17:55:03 +0300

    proelium-exercitum:26/09/2023-17:55:03 automated backup commit

---
+++ leetcode/golang/PalindromeLinkedList.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func isPalindrome(head *ListNode) bool {
+	length = len(head)
+
+	fmt.Println(length)
+
+	//for i := 0; i < length; i++ {
+	//}
+
+	return true
+}
+
+func main() {
+	a := ListNode{Val: 5, Next:}
+
+	//b := ListNode{Val: 5, Next:}
+
+	arr := []ListNode{ }
+
+	isPalindrome()
+}
--- leetcode/golang/PalindromeNumber.go
+++ leetcode/golang/PalindromeNumber.go
@@ -1,11 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func isPalindrome(x int) bool  {
 	if x < 0 {
 		return false
 	}
+
+	str := strconv.Itoa(x)
+	length := len(str)
+
+	for i := 0; i < length/2; i++ {
+		if str[i] != str[length-i-1] {
+			return false
+		}
+	}
+
 	return true
 }
 
